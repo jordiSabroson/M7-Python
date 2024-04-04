a25c8d2a4f8bae9189f67a599e0e998193e37f36
Jordi Manyà <2023_jordi.manya@iticbcn.cat>
2024-04-04 18:12:12 +0200

    consultes avançades i comentaris

--- projecte films - mongodb/Controller/filmDB.py
+++ projecte films - mongodb/Controller/filmDB.py
@@ -2,29 +2,43 @@ from Model.Film import Film
 from db import db_conf
 from datetime import datetime
 from bson import ObjectId
+from fastapi import Query
 
-
-def get_films():
+# Funció get que retorna tots els films i que pot rebre paràmetres per ordenar-los
+def get_films(field: str = Query(None, description="Títol, director o any"),
+              order: str = Query(None, description="Asc o desc"),
+              limit: int = Query(None, description="Límite de registros (1-100)")):
     try:
         # Objecte amb la conexió a la BBDD
         conn = db_conf.client()
-
+        
         # Variable amb la col·lecció films
         collection = conn.films
-
+        
         # Variable que emmagatzema tots els elements de la col·lecció films amb el mètode find()
         all_films = collection.find()
-
-        # Convertir els documents en diccionari
+        
+        # Si se li passem paràmetres, s'ordena la variable amb els films
+        if field and order:
+            all_films = all_films.sort(field, 1 if order == "asc" else -1)
+            
+        # Si li passem un límit, s'aplica
+        if limit:
+            all_films = all_films.limit(limit)
+            
+        # Convertir els documents en diccionari amb l'id del mongo formatada 
         films_data = [film.copy() for film in all_films]
         for film in films_data:
             film["_id"] = str(film["_id"])
-
-        return {"status": 1, "data": films_data}
+            
+        # Recompte de tots els registres
+        row_count = collection.count_documents({})
+        
+        return {"status": 1, "row_count":row_count, "data": films_data}
     except Exception as e:
         return {"status": -1, "missatge": f'{e}'}
 
-
+# Funció que retorna un film per la seva ID del mongoDB
 def get_film_by_id(id):
     try:
         conn = db_conf.client()
@@ -108,11 +122,19 @@ def delete_film(id):
     except Exception as e:
         return {"status": -1, "missatge": f'{e}'}
 
-
-# def base():
-#     try:
-#         conn = db_conf.client()
-#         collection = conn.films
-#         return {"status": 1, "data": "a"}
-#     except Exception as e:
-#         return {"status": -1, "missatge": f'{e}'}
+def consulta_genre(genre):
+    try:
+        conn = db_conf.client()
+        collection = conn.films
+        
+        # Buscar dins la col·lecció segons els gèneres de les pel·lícules
+        films_data = collection.find({"genre": genre})
+        
+        # Passar el resultat a diccionari amb l'id del mongo formatada
+        films_data = [film.copy() for film in films_data]
+        for film in films_data:
+            film["_id"] = str(film["_id"])
+            
+        return {"status": 1, "data": films_data}
+    except Exception as e:
+        return {"status": -1, "missatge": f'{e}'}
--- projecte films - mongodb/main.py
+++ projecte films - mongodb/main.py
@@ -1,37 +1,44 @@
 from fastapi import FastAPI
 from Controller import filmDB
 from Model.Film import Film
+from fastapi import Query
 
 app = FastAPI()
-
 
 @app.get("/")
 def welcome():
     return {"Hola": "que tal"}
 
-
+# Get de tots els films que pot rebre paràmetres per ordenar la resposta
 @app.get("/films/")
-def get_films():
-    return filmDB.get_films()
+def get_films(field: str = Query(None, description="Camp per ordenar (title, director, year)"),
+              order: str = Query(None, description="Ordre (asc, desc)"),
+              limit: int = Query(None, description="Límite de registros (1-100)")):
+    return filmDB.get_films(field, order, limit)
 
-
+# Get dels films per la seva id del mongo
 @app.get("/film/{id}")
 def get_film_by_id(id):
     return filmDB.get_film_by_id(id)
 
-
+# Post per crear films
 @app.post("/film")
 def create_film(film: Film):
     nou_film = filmDB.create_film(film)
     nou_film["data"]["_id"] = str(nou_film["data"]["_id"])
     return nou_film
 
-
+# Put per modificar un film segons la seva id
 @app.put("/film/{id}")
 def update_film(id, film: Film):
     return filmDB.update_film(id, film.model_dump())
 
-
+# Delete per esborrar un film segons la seva id
 @app.delete("/film/{id}")
 def delete_film(id):
     return filmDB.delete_film(id)
+
+# Consulta avançada que utilitza els query parameters per filtrar per gènere
+@app.get("/film_genre")
+def get_film(genre: str = Query(None, description="Gènere de la pel·lícula")):
+    return filmDB.consulta_genre(genre)
